f41883417e825c113cf4f6ef9e698f3a1b35e121
Marquelín <ma.delcanto@duocuc.cl>
2024-06-24 18:39:28 -0400

    Login creado
    Co-authored-by: iscalles <iscalles@users.noreply.github.com>

--- main/forms.py
+++ main/forms.py
@@ -1,10 +1,1 @@
 from django import forms
-
-class crearNombreCliente(forms.Form):
-    nombre_cli = forms.CharField(label=False)
-
-class crearEmailCliente(forms.Form):
-    email_cli = forms.EmailField(label=False)
-
-class crearPasswordCliente(forms.Form):
-    password_cli = forms.CharField(label=False, widget=forms.PasswordInput)
--- main/views.py
+++ main/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
 from .models import Cliente, Pago, Orden, Categoria_producto, Producto, Detalle_carro, Carro_compra
-from .forms import crearNombreCliente
 
 # Create your views here.
 def inicio(request):
     return render(request, 'main/inicio.html')
 
 def login(request):
-    return render(request, 'main/login.html', {
-        'form': crearNombreCliente()
-    })
+    return render(request, 'main/login.html')
 
 def nosotros(request):
     return render(request,'main/sobre_nosotros.html')
