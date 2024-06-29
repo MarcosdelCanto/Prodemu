b84a69166eca8f8c4f9851cf29f514097e762afb
Marquelín <ma.delcanto@duocuc.cl>
2024-06-28 21:31:35 -0400

    PIP FREEZE REQUIREMENTS:TXT

--- cosmetica/settings.py
+++ cosmetica/settings.py
@@ -133,3 +133,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
