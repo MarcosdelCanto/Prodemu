6fc0039800b326d8011b915e6d045f33fa55068b
Marquelín <ma.delcanto@duocuc.cl>
2024-07-17 18:51:05 -0400

    Borre tablas sin ocupar

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Cliente, Pago, Orden, Categoria_producto, Producto, Detalle_carro, Carro_compra
+from .models import Categoria_producto, Producto, Detalle_carro, Carro_compra
 # Register your models here.
 
 class ProductoAdmin(admin.ModelAdmin):
@@ -8,9 +8,6 @@ class ProductoAdmin(admin.ModelAdmin):
     list_filter = ('id_categoria',)
     
 
-admin.site.register(Cliente)
-admin.site.register(Pago)
-admin.site.register(Orden)
 admin.site.register(Categoria_producto)
 admin.site.register(Producto)
 admin.site.register(Detalle_carro)
--- main/models.py
+++ main/models.py
@@ -2,33 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Cliente(models.Model):
-    id_cli = models.AutoField(primary_key=True)
-    nombre_cli = models.CharField(max_length=70)
-    email_cli = models.EmailField(max_length=80)
-    pass_cli = models.CharField(max_length=30)
-    direccion_cli = models.CharField(max_length=200)
-    telefono_cli = models.CharField(max_length=13)
-    
-class Pago(models.Model):
-    id_pago = models.AutoField(primary_key=True)
-    monto_pago = models.IntegerField()
-    fecha_pago = models.DateField()
-    referencia_trans_pago = models.CharField(max_length=20)
-    estado_pago = models.CharField(max_length = 10)
-    
-class Orden(models.Model):
-    id_orden = models.AutoField(primary_key=True)
-    fecha_orden = models.DateField()
-    total_orden = models.IntegerField()
-    estado_orden = models.CharField(max_length=10)
-    id_cli = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    id_pago = models.ForeignKey(Pago, on_delete=models.CASCADE)
 
-    class Meta:
-        verbose_name = 'Orden'
-        verbose_name_plural = 'Ordenes'
-    
 class Categoria_producto(models.Model):
     id_categoria = models.AutoField(primary_key=True)
     descrip_categoria = models.CharField(max_length= 30)
